1e25e8807b58a979b5c9b74e3a111d4a40a5ca31
Kristin Baumann <mail@kristin-baumann.com>
2017-04-20 22:32:08 +0200

    Added first component

---
+++ 2-first-component/2-first-component/src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <h1>Hello First Component!</h1>
+    );
+  }
+}
+
+export default MyComponent;
+
