cd0614129151ef2789b5808019a62c440baa495e
Kristin Baumann <mail@kristin-baumann.com>
2017-04-25 15:35:14 +0200

    Moved component into index file

--- 2-first-component/src/index.js
+++ 2-first-component/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyComponent from './MyComponent';
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <h1>Hello First Component!</h1>
+    );
+  }
+}
 
 ReactDOM.render(
   <MyComponent />,
