812619655c3a11b0e71e7587d45c0d9fd3439d45
Kristin Baumann <mail@kristin-baumann.com>
2017-05-25 22:54:26 +0200

    Add dev tools

--- 12-redux/src/index.js
+++ 12-redux/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import reducers from './reducers';
 
-let store = createStore(reducers);
+
+// create store and use redux dev tool middleware as browser extension
+const store = createStore(
+  reducers,
+  compose(window.devToolsExtension ? window.devToolsExtension() : f => f)
+)
+// or create store without dev tools
+// let store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
