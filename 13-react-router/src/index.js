0dc0b6caf8dfda58552d437281fa6b1363a2f5f9
Kristin Baumann <mail@kristin-baumann.com>
2017-05-31 23:32:30 +0200

    Fixes

--- 13-react-router/src/index.js
+++ 13-react-router/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <BrowserRouter>
