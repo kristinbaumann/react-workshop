d0e2ca1be709f5797b3666b1ff207dd2d2041a0d
Kristin Baumann <mail@kristin-baumann.com>
2017-06-03 18:55:08 +0200

    Added first conference example - step0

---
+++ 15-conference-application/step0-initial/src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from '../../img/logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="app-intro">
+          To get started, edit <code>src/components/App/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
